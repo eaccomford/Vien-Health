b10af6c465332e9cd50fddfdb030eb53fbd2c649
Emmanuel Accomford <eaccomford@gmail.com>
2021-05-07 13:12:20 +0000

    cleaned up the route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import {Router} from "express"
 import auth from "./user"
-import dashboard from "./dashboard"
 
 const router = Router()
 
 router.use('/user', auth)
-router.use('/dashboard', dashboard)
 
 export default router
