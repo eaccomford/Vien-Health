b1b9633027df6e8283566ff3287141b6681a89d0
Emmanuel Accomford <eaccomford@gmail.com>
2021-05-07 13:11:20 +0000

    validated username for registration

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,17 +1,14 @@
-import logging from 'config/logging';
 import mongoose, {Schema} from 'mongoose'
 import UserInterface from '../interfaces/userInterface'
 
 const UserSchema: Schema = new Schema({
-    username: { type: String, required: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    username: { type: String, required: true},
     password: { type: String, required: true },
 },
 {
     timestamps: true
 })
-
-// UserSchema.post<UserInterface>('save', function () {
-//     logging.info('Mongo', 'Checkout the book we just saved: ', this);
-// });
 
 export default mongoose.model<UserInterface>('User', UserSchema)
